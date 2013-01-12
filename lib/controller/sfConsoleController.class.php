c508eafd326f41dcad5521598bfdd1d39b11da26
Mishal <michi.m@gmail.com>
2013-01-12 22:19:50 +0100

    adding controller

---
+++ lib/controller/sfConsoleController.class.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This file is part of the Sift PHP framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @package    Sift
+ * @subpackage controller
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com> 
+ */
+class sfConsoleController extends sfController
+{
+  /**
+   * Dispatches a request.
+   *
+   * @param string A module name
+   * @param string An action name
+   * @param array  An associative array of parameters to be set
+   */
+  public function dispatch($moduleName, $actionName, $parameters = array())
+  {
+    try
+    {
+      // set parameters
+      $this->getContext()->getRequest()->getParameterHolder()->add($parameters);
+
+      // make the first request
+      $this->forward($moduleName, $actionName);
+    }
+    catch (sfException $e)
+    {
+      $e->printStackTrace();
+    }
+    catch (Exception $e)
+    {
+      // wrap non symfony exceptions
+      $sfException = new sfException();
+      $sfException->printStackTrace($e);
+    }
+  }
+}
