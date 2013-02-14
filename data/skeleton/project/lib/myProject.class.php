9f445da672bd9f5adad8df86400e827a95d661d1
Mishal <michi.m@gmail.com>
2013-02-14 13:03:32 +0100

    added configure method to project skeleton class

--- data/skeleton/project/lib/myProject.class.php
+++ data/skeleton/project/lib/myProject.class.php
@@ -1,24 +1,34 @@
 <?php
 
 /**
- * 
+ *
  * @package ##PROJECT_NAME##
  * @subpackage project
  */
 class myProject extends sfProject {
-  
+
   /**
    * Project version
    */
   const VERSION = '1.0.0';
 
   /**
-   * 
+   *
    * @return string
    */
   public static function getVersion()
   {
     return self::VERSION;
   }
- 
+
+  /**
+   * Configures the project
+   *
+   */
+  public function configure()
+  {
+    // place custom listeners here
+    // $this->getEventDispatcher()->connect('event', array($this, 'listenToEvent'));
+  }
+
 }
