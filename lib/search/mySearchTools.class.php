7d248044278402e094a28f80dc6708001311b5de
Mishal <michi.m@gmail.com>
2013-02-14 11:33:48 +0100

    added mySearchTools class

---
+++ lib/search/mySearchTools.class.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the Sift PHP framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * mySearchTools - custom extensions
+ *
+ * @package Sift
+ * @subpackage search
+ */
+class mySearchTools extends sfSearchTools {}
