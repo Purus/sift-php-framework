987ab5249876287f59448f3064658722f63161a1
Mishal <michi.m@gmail.com>
2013-02-25 22:30:39 +0100

    refactored sfAssetPackage

--- lib/util/sfAssetPackage.class.php
+++ lib/util/sfAssetPackage.class.php
@@ -7,7 +7,9 @@
  */
 
 /**
- * sfAssetPackage class
+ * sfAssetPackage class provides a configuration object for asset packages.
+ * Assets are registered in aseet_packages.yml configuration and can be altered
+ * via event system event name "asset_packages.get_config".
  *
  * @package    Sift
  * @subpackage util
@@ -16,20 +18,20 @@ class sfAssetPackage {
 
   /**
    * Configuration holder
-   * 
+   *
    * @var array
    */
   static $config = array();
 
   /**
    * Has event system already touched configuration?
-   * 
-   * @var type boolean 
+   *
+   * @var boolean
    */
   static $filtered = false;
-  
+
   /**
-   * Returns configuration array (filtered by event listeners to event 
+   * Returns configuration array (filtered by event listeners to event
    * "asset_packages.get_config"
    *
    * @return array Configuration array
@@ -39,9 +41,9 @@ class sfAssetPackage {
     if(!self::$filtered)
     {
       self::$config = sfCore::filterByEventListeners(
-                        self::$config, 'asset_packages.get_config');
-      self::$filtered = true;      
-    }    
+                      self::$config, 'asset_packages.get_config');
+      self::$filtered = true;
+    }
     return self::$config;
   }
 
@@ -69,58 +71,34 @@ class sfAssetPackage {
       $name = array($name);
     }
 
-    $config = self::getConfig();
-    
     $result = array();
     foreach($name as $src)
     {
-      if(!isset($config['packages'][$src]['javascripts']))
+      if(!self::hasPackage($src))
       {
-        if($config['strict_mode'])
-        {
-          throw new sfException(sprintf('{sfAssetPackage} Unknown package name "%s" or no javascript files configured for this package.', $src));
-        }
-        else
-        {
-          // quess package name, we assume its jquery plugin
-          $config['packages'][$src]['javascripts'] = 
-            sfConfig::get('sf_environment') == 'dev' ?
-              array(sprintf('%1$s/jquery.%1$s.js', $src)) :
-              // minimised version in other then dev environments
-              array(sprintf('%1$s/jquery.%1$s.min.js', $src));                    
-        }
+        throw new sfException(sprintf('{sfAssetPackage} Unknown package name "%s" or no javascript files configured for this package.', $src));
       }
 
+      $javascripts = sfArray::get(self::getConfig(), sprintf('packages.%s.javascripts', $src), array());
       if($includeRequired)
       {
         // we include required before
         $result = array_merge($result, self::getRequiredJavascripts($src));
       }
-
-      // unset javascripts which are excluded
-      if(isset($config['packages'][$src]['i18n']['exclude']))
-      {
-        foreach($config['packages'][$src]['javascripts'] as $i => $js)
-        {
-          $culture = sfContext::getInstance()->getUser()->getCulture();
-          if(preg_match('~%SF_CULTURE({(\d)+,(\d)+})?%~', $js, $matches))
-          {
-            if(isset($matches[2]) && isset($matches[3]))
-            {
-              $culture = substr($culture, $matches[2], $matches[3]);
-            }                      
-          }
-          if(stripos($js, '%SF_CULTURE') !== false 
-              && in_array($culture, (array)$config['packages'][$src]['i18n']['exclude']))
-          {
-            unset($config['packages'][$src]['javascripts'][$i]);
-          }
-        }
-      }
-      $result = array_merge($result, array_map(array('sfAssetPackage', 'replaceVariables'), 
-                            $config['packages'][$src]['javascripts']));
+      $result = array_merge($result, $javascripts);
     }
-    return $result;
+    return self::replaceVariables($result);
+  }
+
+  /**
+   * Is package present on the configuration?
+   *
+   * @param string $name Name of the package
+   * @return boolean
+   */
+  public static function hasPackage($name)
+  {
+    return sfArray::keyExists(self::getConfig(), sprintf('packages.%s', $name));
   }
 
   /**
@@ -132,68 +110,57 @@ class sfAssetPackage {
    */
   public static function getStylesheets($name, $includeRequired = true)
   {
-    $config = self::getConfig();
-    
     if(!is_array($name))
     {
       $name = array($name);
     }
-    
+
     $result = array();
+
     foreach($name as $src)
     {
-      if(!isset($config['packages'][$src]['stylesheets']))
+      if(!self::hasPackage($src))
       {
-        if($config['strict_mode'])
-        {      
-          throw new sfException(sprintf('{sfAssetPackage} Unknown package name "%s" or no stylesheets files configured for this package.', $src));
-        }
-        else
+        throw new sfException(sprintf('{sfAssetPackage} Unknown package name "%s".', $src));
+      }
+
+      $stylesheets = sfArray::get(self::getConfig(), sprintf('packages.%s.stylesheets', $src), array());
+
+      if($includeRequired)
+      {
+        // get required packages
+        foreach(sfArray::get(self::getConfig(), sprintf('packages.%s.require', $src), array()) as $s)
         {
-          // no stylesheets 
-          $config['packages'][$src]['stylesheets'] = array();
+          $result = array_merge($result, self::getStylesheets($s));
         }
       }
-      if($includeRequired && isset($config['packages'][$src]['require']))
-      {
-        foreach($config['packages'][$src]['require'] as $s)
-        {
-          $result = array_merge($result, self::getStylesheets($s, false));
-        }        
-      }      
-      $result =  array_merge($result, array_map(array('sfAssetPackage', 'replaceVariables'),
-              $config['packages'][$src]['stylesheets']));      
-    }    
-    
-    return $result;
+      $result = array_merge($result, $stylesheets);
+    }
+
+    return self::replaceVariables($result);
   }
 
   /**
    * Returns other packages javascripts required by package
-   * 
+   *
    * @param type $src
    * @return type array
    */
   public static function getRequiredJavascripts($src)
   {
-    $config = self::getConfig();
-    if(isset($config['packages'][$src]['require']))
-    {
-      $require = (array)$config['packages'][$src]['require']; 
-      return self::getJavascripts($require);
-    }
-    return array();    
+    return self::getJavascripts(
+                    sfArray::get(self::getConfig(), sprintf('packages.%s.require', $src), array())
+    );
   }
-  
+
   /**
    * Returns all configured packages
-   * 
+   *
    * @return array
    */
   public static function getAllPackages()
   {
-    $config = self::getConfig();
-    return isset($config['packages']) ? (array)$config['packages'] : array();
+    return sfArray::get(self::getConfig(), 'packages', array());
   }
 
   /**
@@ -203,53 +170,62 @@ class sfAssetPackage {
    * %SF_CULTURE%      -> current culture
    * %SF_JAVASCRIPT_PATH% -> javascript path /js (takes care also for relative_root)
    * %SF_STYLESHEET_PATH  -> stylesheet path /css (takes care also for relative_root)
-   * %SF_JQUERY_WEB_DIR%  -> web directory where jquery sits
    *
    * @param string $value
+   * @param string key
    * @return string
    */
-  protected static function replaceVariables($value)
+  public static function replaceVariables(&$value)
   {
-    // %SF_CULTURE{0,2}% -> substr($culture, 0, 2)
-    // %SF_CULTURE%
-    if(preg_match('~%SF_CULTURE({(\d)+,(\d)+})?%~', $value, $matches))
+    if(is_array($value))
     {
-      $culture = sfContext::getInstance()->getUser()->getCulture();
-      if(isset($matches[2]) && isset($matches[3]))
+      foreach($value as $k => $v)
       {
-        $culture = substr($culture, $matches[2], $matches[3]);
-      }
-      $value = preg_replace('~%SF_CULTURE(.*)?%~', $culture, $value);      
-    }
-
-    $request = sfContext::getInstance()->getRequest();
-    $sf_relative_url_root = $request->getRelativeUrlRoot();
-
-    $value = str_replace(
-    array(
-      '%SF_JAVASCRIPT_PATH%', // BC compat
-      '%SF_STYLESHEET_PATH%', // BC compat
-      '%SF_JAVASCRIPT_WEB_PATH%',
-      '%SF_STYLESHEET_WEB_PATH%',
-    ),
-    array(
-      $sf_relative_url_root.'/js',
-      $sf_relative_url_root.'/css',
-      $sf_relative_url_root.'/js',
-      $sf_relative_url_root.'/css'        
-    ), $value);
-
-    // absolute path or protocol
-    if(strpos($value, '/') !== 0 && strpos($value, '://') == false)
-    {
-      // we skip stylesheets, these reside in /css directory
-      if(strpos($value, '.css') === false)
-      {
-        // $value = sprintf('%s/%s', self::getPackagePath(), $value);
+        if(is_numeric($k))
+        {
+          $value[$k] = self::replaceVariables($value[$k]);
+        }
+        elseif(is_string($k))
+        {
+          $tmp = self::replaceVariables($value[$k]);
+          unset($value[$k]);
+          $value[self::replaceVariables($k)] = $tmp;
+        }
       }
     }
-    
+    elseif(is_string($value))
+    {
+      $value = sfToolkit::replaceConstants($value);
+      // %SF_CULTURE{0,2}% -> substr($culture, 0, 2)
+      // %SF_CULTURE%
+      if(preg_match('~%SF_CULTURE({(\d)+,(\d)+})?%~', $value, $matches))
+      {
+        $culture = sfContext::getInstance()->getUser()->getCulture();
+        if(isset($matches[2]) && isset($matches[3]))
+        {
+          $culture = substr($culture, $matches[2], $matches[3]);
+        }
+        $value = preg_replace('~%SF_CULTURE(.*)?%~', $culture, $value);
+      }
+
+      $request = sfContext::getInstance()->getRequest();
+      $sf_relative_url_root = $request->getRelativeUrlRoot();
+
+      $value = str_replace(
+              array(
+          '%SF_JAVASCRIPT_PATH%', // BC compat
+          '%SF_STYLESHEET_PATH%', // BC compat
+          '%SF_JAVASCRIPT_WEB_PATH%',
+          '%SF_STYLESHEET_WEB_PATH%',
+              ), array(
+          $sf_relative_url_root . '/js',
+          $sf_relative_url_root . '/css',
+          $sf_relative_url_root . '/js',
+          $sf_relative_url_root . '/css'
+              ), $value);
+    }
+
     return $value;
   }
-  
+
 }
---
+++ test/unit/util/sfAssetPackageTest.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
+require_once(dirname(__FILE__).'/../sfContextMock.class.php');
+require_once(dirname(__FILE__).'/../sfCoreMock.class.php');
+
+
+$t = new lime_test(14, new lime_output_color());
+
+class myAssetPackage extends sfAssetPackage {
+
+  public static function getConfig()
+  {
+    return self::$config;
+  }
+}
+
+sfConfig::set('sf_sift_web_dir', '/sf/foobar');
+
+$config = sfYaml::load(dirname(__FILE__) . '/fixtures/asset_packages.yml');
+
+myAssetPackage::setConfig($config['default']);
+
+$t->isa_ok(myAssetPackage::getConfig(), 'array', '::getConfig() returns array');
+
+$t->isa_ok(myAssetPackage::getJavascripts('date_picker'), 'array', '::getJavascripts() returns array');
+$t->isa_ok(myAssetPackage::getStylesheets('date_picker'), 'array', '::getStylesheets() returns array');
+
+$t->isa_ok(myAssetPackage::getAllPackages(), 'array', '::getAllPackages() returns array');
+$t->is(count(myAssetPackage::getAllPackages()), 15, '::getAllPackages() returns packages');
+
+$t->isa_ok(myAssetPackage::hasPackage('ui'), 'boolean', '::hasPackage() returns boolean');
+$t->is(myAssetPackage::hasPackage('ui'), true, '::hasPackage() returns boolean');
+$t->is(myAssetPackage::hasPackage('invalid_package'), false, '::hasPackage() returns boolean');
+
+$t->is(myAssetPackage::getJavascripts('date_picker'), array(
+    '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js',
+    '/sf/foobar/js/bootstrap/bootstrap-datetimepicker.js',
+    '/sf/foobar/js/bootstrap/locales/bootstrap-datetimepicker.en.js'
+), '::getJavascripts() returns array');
+
+$t->is(myAssetPackage::getStylesheets('date_picker'), array(
+    0 => array(
+      '/sf/foobar/css/_/bootstrap/datetimepicker.print.less' => array('media' => 'print')
+    ),
+    1 => array(
+      '/sf/foobar/css/_/bootstrap/datetimepicker.less' => array('media' => 'screen,projection,tv')
+    )
+), '::getStylesheets() returns array');
+
+
+$t->is(myAssetPackage::getJavascripts('validation'), array(
+  0 => '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js',
+  1 => '/sf/foobar/js/validate/jquery.validate.min.js',
+  2 => '/sf/foobar/js/validate/additional-methods.min.js',
+  3 => '/sf/foobar/js/validate/jquery.validate.custom_callback.min.js',
+  4 => '/sf/foobar/js/validate/localization/messages_en.js',
+), '::getJavascripts() returns array');
+
+$t->is(myAssetPackage::getStylesheets('validation'), array(), '::getStylesheets() returns array');
+
+$t->is(myAssetPackage::getJavascripts('dynamic_api'), array(
+  0 => '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js',
+  1 =>
+  array (
+    '/sf/foobar/js/hover_intent/jquery.hoverIntent.minified.js' =>
+    array (
+      'generated' => true,
+    ),
+  )
+), '::getJavascripts() returns array');
+
+
+$t->is(myAssetPackage::getJavascripts('fancybox'), array(
+  0 => '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js',
+  1 => '/js/jquery/fancybox/jquery.fancybox-1.3.4.js',
+  2 =>
+  array (
+    '/js/jquery/fancybox/ie_hacks.js' =>
+    array (
+      'ie_condition' => 'lte IE 10',
+    ),
+  ),
+), '::getJavascripts() returns array');
+
+// http://trac.symfony-project.org/attachment/ticket/7588/AssetHelperPatch.diff
+// http://exacttarget.github.com/fuelux/#spinner
