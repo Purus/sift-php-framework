d4d39ea683e8652fbb9d42670e79ce12389eb9e6
Mishal <michi.m@gmail.com>
2013-03-02 14:58:05 +0100

    updated check script

--- data/bin/check.php
+++ data/bin/check.php
@@ -72,7 +72,7 @@ if (is_cli())
 // mandatory
 echo "\n** Mandatory requirements **\n\n";
 
-check(version_compare(phpversion(), '5.2.3', '>='), 'PHP version is at least 5.2.3', 'Current version is '.phpversion(), true);
+check(version_compare(phpversion(), '5.2.3', '>='), sprintf('PHP version is at least 5.2.3 (Current: %s)', phpversion()), 'Current version is '.phpversion(), true);
 check(class_exists('PDO'), 'PDO is installed', 'Install PDO (mandatory for Doctrine)', true);
 if(class_exists('PDO'))
 {
@@ -87,8 +87,10 @@ check(function_exists('mb_strlen'), 'The mb_strlen() function is available', 'In
 check(function_exists('iconv'), 'The iconv() function is available', 'Install iconv() function', false);
 check(function_exists('utf8_decode'), 'The utf8_decode() is available', 'Install utf8_decode() function', false);
 check(extension_loaded('gd') && function_exists('gd_info'), 'GD graphics library is installed', 'Install GD graphics library', true);
+check(extension_loaded('mcrypt') && function_exists('mcrypt_module_open'), 'MCrypt module is installed', 'Install Mcrypt module', true);
+check(extension_loaded('intl') && class_exists('Collator'), 'Intl module is installed', 'Install intl module (Locale comparison methods like sorting may not work ok.)', false);
 
-$accelerator = 
+$accelerator =
   (function_exists('apc_store') && ini_get('apc.enabled'))
   ||
   function_exists('eaccelerator_put') && ini_get('eaccelerator.enable')
@@ -105,14 +107,41 @@ check(!ini_get('safe_mode'), 'safe mode is turned off', 'Set it to off in php.in
 
 check(version_compare(phpversion(), '5.2.9', '!='), 'PHP version is not 5.2.9', 'PHP 5.2.9 broke array_unique() and sfToolkit::arrayDeepMerge(). Use 5.2.10 instead [Ticket #6211]', false);
 
-// print_r(get_loaded_extensions());
+echo "\n\n** Other settings: **\n\n";
 
-// var_dump(ini_get('memory_limit'));
-// var_dump(ini_get('max_execution_time'));
-// var_dump(ini_get('post_max_size'));
+echo sprintf('  Open base dir directive is: %s', ini_get('open_basedir')) . "\n";
+echo sprintf('  Disabled functions: %s', ini_get('disable_functions')) . "\n";
+echo sprintf('  Disabled classes: %s', ini_get('disable_classes')) . "\n";
+echo sprintf('  Memory limit: %s', ini_get('memory_limit')) . "\n";
+echo sprintf('  Max execution time: %s', ini_get('max_execution_time')) . "\n";
+echo sprintf('  Post max size: %s', ini_get('post_max_size')) . "\n";
 
-// var_dump(explode(',', ini_get('disable_functions')));
-// var_dump(gd_info());
+echo "\n** Loaded extensions: **\n\n";
+
+$extensions = get_loaded_extensions();
+asort($extensions);
+
+$i = 1;
+foreach($extensions as $extension)
+{
+  echo sprintf('  %s', $extension);
+  if($i % 10 == 0)
+  {
+    echo "\n";
+  }
+  $i++;
+}
+
+if(function_exists('gd_info'))
+{
+  echo "\n\n** GD library: **\n\n";
+  foreach(gd_info() as $k => $v)
+  {
+    echo sprintf('  %s: %s', $k,
+            is_bool($v) ? ($v ? 'true' : 'false') : $v)
+                     . "\n";
+  }
+}
 
 if(!is_cli())
 {
