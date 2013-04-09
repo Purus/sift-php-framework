87158be41c0a69f71a37049740d2437b2adea7c0
Mishal <michi.m@gmail.com>
2013-04-09 06:59:15 +0200

    updated ajax result, implemented static create method

--- lib/response/sfAjaxResult.class.php
+++ lib/response/sfAjaxResult.class.php
@@ -9,29 +9,29 @@
 /**
  * sfAjaxResult class is standardized object for communication with client side
  * javascript
- * 
+ *
  * @package Sift
  * @subpackage response
  */
 class sfAjaxResult {
-  
+
   /**
    * Success or error flag
-   * 
-   * @var boolean 
+   *
+   * @var boolean
    */
   public $success = true;
-  
+
   /**
    * Result HTML code
    *
-   */ 
+   */
   public $html = '';
-  
+
   /**
    * Constructs new object
-   * 
-   * @param boolean $success 
+   *
+   * @param boolean $success
    * @param string $html HTML result
    */
   public function __construct($success = true, $html = '')
@@ -41,21 +41,33 @@ class sfAjaxResult {
   }
 
   /**
-   * Converts object instance to JSON 
-   * 
+   * Converts object instance to JSON
+   *
    * @param boolean Force to object (uses JSON_FORCE_OBJECT parameter)
    * @return string
    */
   public function toJson($forceObject = false)
-  {    
+  {
     return $forceObject ? sfJson::encode($this, true, JSON_FORCE_OBJECT) : sfJson::encode($this);
   }
-  
+
+  /**
+   * Creates the result from array
+   *
+   * @param array $array
+   */
+  public static function createFromArray($array)
+  {
+    $result = new self();
+    $result->fromArray($array);
+    return $result;
+  }
+
   /**
    * Creates object properties from associative array
-   * 
+   *
    * @param array $array
-   * @return sfAjaxResult 
+   * @return sfAjaxResult
    */
   public function fromArray($array)
   {
@@ -65,5 +77,16 @@ class sfAjaxResult {
     }
     return $this;
   }
-    
+
+  /**
+   * Converts the object to string
+   *
+   * @return string
+   * @see toJson()
+   */
+  public function __toString()
+  {
+    return $this->toJson();
+  }
+
 }
