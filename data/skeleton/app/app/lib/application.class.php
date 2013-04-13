84be8b21fec4d685e8fdeef511e1542470dce37a
Mishal <michi.m@gmail.com>
2013-04-13 09:04:28 +0200

    updated the application skeleton

--- data/skeleton/app/app/lib/application.class.php
+++ data/skeleton/app/app/lib/application.class.php
@@ -1,18 +1,30 @@
 <?php
 
 /**
- * 
+ *
  * @package ##PROJECT_NAME##
  * @subpackage project
  */
 class ##CLASS_NAME## extends sfApplication {
 
   /**
-   * Easly stage dimension initialization. Beware that this is called before 
-   * the sfContext gets created, so there is no access to internal objects.  
+   * Early stage application dimension initialization.
+   *
+   * Beware that this is called before the sfContext gets created,
+   * so there is no access to internal objects.
    */
   public function initCurrentDimension()
   {
   }
+
+  /**
+   * Initializes the application.
+   *
+   */
+  // public function initialize()
+  // {
+  //  // Include custom configuration file
+  //  // include $this->getConfigCache()->checkConfig('config/custom.yml');
+  // }
 
 }
