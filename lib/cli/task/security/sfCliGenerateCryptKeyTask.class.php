32f928294d22f6b667a52f128e2b7b60f24e3034
Mishal <michi.m@gmail.com>
2013-06-15 09:50:56 +0200

    fixed generate crypt key task

--- lib/cli/task/security/sfCliGenerateCryptKeyTask.class.php
+++ lib/cli/task/security/sfCliGenerateCryptKeyTask.class.php
@@ -20,7 +20,7 @@ class sfCliGenerateCryptKeyTask extends sfCliBaseTask
   protected function configure()
   {
     $this->addArguments(array(
-      new sfCliCommandArgument('application', sfCliCommandArgument::OPTIONAL, 'The application name'),
+      new sfCliCommandArgument('app', sfCliCommandArgument::OPTIONAL, 'The application name'),
     ));
 
     $this->namespace = 'security';
@@ -51,10 +51,10 @@ EOF;
     $filesystem = $this->getFilesystem();
 
     $app = false;
-    if(isset($arguments['application']))
+    if(isset($arguments['app']))
     {
-      $this->checkAppExists($arguments['application']);
-      $app = $arguments['application'];
+      $this->checkAppExists($arguments['app']);
+      $app = $arguments['app'];
     }
 
     $this->logSection($this->getFullName(), 'Generating crypt key...');
@@ -108,97 +108,4 @@ EOF;
     $this->logSection($this->getFullName(), 'Done.');
   }
 
-  /**
-   * Rotates log file.
-   *
-   * @param  string $app       Application name
-   * @param  string $env       Enviroment name
-   * @param  string $period    Period
-   * @param  string $history   History
-   * @param  bool   $override  Override
-   *
-   **/
-  public function rotate($app, $env, $period = null, $history = null, $override = false)
-  {
-    $logfile = $app.'_'.$env;
-    $logdir = $this->environment->get('sf_log_dir');
-
-    // set history and period values if not passed to default values
-    $period = isset($period) ? $period : self::DEF_PERIOD;
-    $history = isset($history) ? $history : self::DEF_HISTORY;
-
-    // get todays date
-    $today = date('Ymd');
-
-    // check history folder exists
-    if (!is_dir($logdir.'/history'))
-    {
-      $this->getFilesystem()->mkdirs($logdir.'/history');
-    }
-
-    // determine date of last rotation
-    $logs = sfFinder::type('file')->maxdepth(1)->name($logfile.'_*.log')->sort_by_name()->in($logdir.'/history');
-    $recentlog = is_array($logs) ? array_pop($logs) : null;
-
-    if ($recentlog)
-    {
-      // calculate date to rotate logs on
-      $lastRotatedOn = filemtime($recentlog);
-      $rotateOn = date('Ymd', strtotime('+ '.$period.' days', $lastRotatedOn));
-    }
-    else
-    {
-      // no rotation has occured yet
-      $rotateOn = null;
-    }
-
-    $srcLog = $logdir.'/'.$logfile.'.log';
-    $destLog = $logdir.'/history/'.$logfile.'_'.$today.'.log';
-
-    // if rotate log on date doesn't exist, or that date is today, then rotate the log
-    if (!$rotateOn || ($rotateOn == $today) || $override)
-    {
-      // create a lock file
-      $lockFile = $this->environment->get('sf_root_dir').'/'.$app.'_'.$env.'-cli.lck';
-      $this->getFilesystem()->touch($lockFile);
-
-      // change mode so the web user can remove it if we die
-      $this->getFilesystem()->chmod($lockFile, 0777);
-
-      // if log file exists rotate it
-      if(file_exists($srcLog))
-      {
-        // check if the log file has already been rotated today
-        if (file_exists($destLog))
-        {
-          // append log to existing rotated log
-          $handle = fopen($destLog, 'a');
-          $append = file_get_contents($srcLog);
-
-          $this->logSection('file+', $destLog);
-          fwrite($handle, $append);
-        }
-        else
-        {
-          // copy log
-          $this->getFilesystem()->copy($srcLog, $destLog);
-        }
-
-        // remove the log file
-        $this->getFilesystem()->remove($srcLog);
-
-        // get all log history files for this application and environment
-        $newLogs = sfFinder::type('file')->maxdepth(1)->name($logfile.'_*.log')->sort_by_name()->in($logdir.'/history');
-
-        // if the number of logs in history exceeds history then remove the oldest log
-        if (count($newLogs) > $history)
-        {
-          $this->getFilesystem()->remove($newLogs[0]);
-        }
-      }
-
-      // release lock
-      $this->getFilesystem()->remove($lockFile);
-    }
-  }
 }
