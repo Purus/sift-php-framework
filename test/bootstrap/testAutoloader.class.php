239408f47d482104ac4dfe3d95fba9b20e85b0df
Mishal <michi.m@gmail.com>
2013-08-08 09:16:08 +0200

    updated testAutoloader - make the classname case insensitive

--- test/bootstrap/testAutoloader.class.php
+++ test/bootstrap/testAutoloader.class.php
@@ -1,46 +1,47 @@
 <?php
 
-require_once(dirname(__FILE__).'/../../lib/util/sfFinder.class.php');
-require_once(dirname(__FILE__).'/../../lib/util/sfGlobToRegex.class.php');
-require_once(dirname(__FILE__).'/../../lib/util/sfNumberCompare.class.php');
-require_once(dirname(__FILE__).'/../../lib/util/sfToolkit.class.php');
+require_once(dirname(__FILE__) . '/../../lib/util/sfFinder.class.php');
+require_once(dirname(__FILE__) . '/../../lib/util/sfGlobToRegex.class.php');
+require_once(dirname(__FILE__) . '/../../lib/util/sfNumberCompare.class.php');
+require_once(dirname(__FILE__) . '/../../lib/util/sfToolkit.class.php');
 
-class testAutoloader
-{
+class testAutoloader {
+
   static public $class_paths = array();
 
   static public function initialize($with_cache = true)
   {
     $tmp_dir = sfToolkit::getTmpDir();
-    if (is_readable($tmp_dir.DIRECTORY_SEPARATOR.'sf_autoload_paths.php'))
-    {      
-      self::$class_paths = unserialize(file_get_contents($tmp_dir.DIRECTORY_SEPARATOR.'sf_autoload_paths.php'));
+    if(is_readable($tmp_dir . DIRECTORY_SEPARATOR . 'sf_autoload_paths.php'))
+    {
+      self::$class_paths = unserialize(file_get_contents($tmp_dir . DIRECTORY_SEPARATOR . 'sf_autoload_paths.php'));
     }
     else
     {
-      $files = sfFinder::type('file')->name('*.php')->ignore_version_control()->in(realpath(dirname(__FILE__).'/../../lib'));
+      $files = sfFinder::type('file')->name('*.php')->ignore_version_control()->in(realpath(dirname(__FILE__) . '/../../lib'));
       self::$class_paths = array();
-      foreach ($files as $file)
+      foreach($files as $file)
       {
         preg_match_all('~^\s*(?:abstract\s+|final\s+)?(?:class|interface)\s+(\w+)~mi', file_get_contents($file), $classes);
-        foreach ($classes[1] as $class)
+        foreach($classes[1] as $class)
         {
+          $class = strtolower($class);
           self::$class_paths[$class] = $file;
         }
       }
-      if ($with_cache)
+      if($with_cache)
       {
-        file_put_contents($tmp_dir.DIRECTORY_SEPARATOR.'sf_autoload_paths.php', serialize(self::$class_paths));
+        file_put_contents($tmp_dir . DIRECTORY_SEPARATOR . 'sf_autoload_paths.php', serialize(self::$class_paths));
       }
     }
   }
 
   static public function __autoload($class)
   {
-    if (isset(self::$class_paths[$class]))
+    $class = strtolower($class);
+    if(isset(self::$class_paths[$class]))
     {
       require(self::$class_paths[$class]);
-
       return true;
     }
 
@@ -49,6 +50,7 @@ class testAutoloader
 
   static public function removeCache()
   {
-    unlink(sfToolkit::getTmpDir().DIRECTORY_SEPARATOR.'sf_autoload_paths.php');
+    unlink(sfToolkit::getTmpDir() . DIRECTORY_SEPARATOR . 'sf_autoload_paths.php');
   }
+
 }
