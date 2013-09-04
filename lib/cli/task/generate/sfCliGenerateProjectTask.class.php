b4f8ff94bf7f268f24f0779414769cb4ca978d5f
Mishal <michi.m@gmail.com>
2013-09-04 08:50:15 +0200

    fixed #24

--- lib/cli/task/generate/sfCliGenerateProjectTask.class.php
+++ lib/cli/task/generate/sfCliGenerateProjectTask.class.php
@@ -31,20 +31,21 @@ class sfCliGenerateProjectTask extends sfCliGeneratorBaseTask
   {
     $this->addArguments(array(
       new sfCliCommandArgument('name', sfCliCommandArgument::REQUIRED, 'The project name'),
+      new sfCliCommandArgument('webmaster_email', sfCliCommandArgument::REQUIRED, 'The webmaster email'),
       new sfCliCommandArgument('author', sfCliCommandArgument::OPTIONAL, 'The project author', 'Your name here'),
     ));
 
     $this->addOptions(array(
       new sfCliCommandOption('no-git-stuff', null, sfCliCommandOption::PARAMETER_NONE, 'Do not add git specific files to the project directory?', null)
     ));
-    
+
     $this->namespace = 'generate';
     $this->name = 'project';
 
     $this->briefDescription = 'Generates a new project';
 
     $scriptName = $this->environment->get('script_name');
-    
+
     $this->detailedDescription = <<<EOF
 The [generate:project|INFO] task creates the basic directory structure
 for a new project in the current directory:
@@ -54,10 +55,13 @@ for a new project in the current directory:
 If the current directory already contains a Sift project,
 it throws a [sfCliCommandException|COMMENT].
 
-You can optionally include a second [author|COMMENT] argument to specify what name to
+You have to specify the [webmaster email|COMMENT] as second argument. This is used in
+500 error pages. This can be modified later in the project app.yml configuration file.
+
+You can optionally include a third [author|COMMENT] argument to specify what name to
 use as author when framework generates new modules and other generated stuff:
 
-  [{$scriptName} generate:project blog "Jack Doe"|INFO]
+  [{$scriptName} generate:project blog jack@my-project.com "Jack Doe" |INFO]
 EOF;
   }
 
@@ -66,8 +70,8 @@ EOF;
    */
   protected function execute($arguments = array(), $options = array())
   {
-    try 
-    {      
+    try
+    {
       $this->checkProjectExists();
       throw new sfCliCommandArgumentsException('Sift project already exits in current directory');
     }
@@ -78,26 +82,27 @@ EOF;
     catch(sfException $e)
     {
     }
-    
+
     $this->arguments = $arguments;
     $this->options = $options;
-    
+
     $this->logSection($this->getFullName(), 'Creating project...');
-    
+
     // create basic project structure
     $this->installDir($this->environment->get('sf_sift_data_dir').'/skeleton/project');
 
     if(isset($options['nogitstuff']))
     {
       $files = sfFinder::type('file')->name('.gitignore')->level(0)->in($this->environment->get('sf_root_dir'));
-      $this->getFilesystem()->remove($files);   
-    }  
-    
+      $this->getFilesystem()->remove($files);
+    }
+
     $this->tokens = array(
-      'SF_SIFT_LIB_DIR'   => $this->environment->get('sf_sift_lib_dir'),  
-      'SF_SIFT_DATA_DIR'  => $this->environment->get('sf_sift_data_dir'),  
+      'SF_SIFT_LIB_DIR'   => $this->environment->get('sf_sift_lib_dir'),
+      'SF_SIFT_DATA_DIR'  => $this->environment->get('sf_sift_data_dir'),
       'PROJECT_NAME'      => $this->arguments['name'],
       'AUTHOR_NAME'       => $this->arguments['author'],
+      'WEBMASTER_EMAIL'   => $this->arguments['webmaster_email'],
       'PROJECT_DIR'       => $this->environment->get('sf_root_dir'),
       'IS_SECURE'         => isset($options['secured']) ? 'true' : 'false'
     );
@@ -106,13 +111,13 @@ EOF;
 
     // project is generated, we have to bind to project
     $this->commandApplication->bindToProject();
-    
-    try 
+
+    try
     {
       // generate new crypt key for the project
-      $generateCryptKey = new sfCliGenerateCryptKeyTask($this->environment, 
-                                                        $this->dispatcher, 
-                                                        $this->formatter, 
+      $generateCryptKey = new sfCliGenerateCryptKeyTask($this->environment,
+                                                        $this->dispatcher,
+                                                        $this->formatter,
                                                         $this->logger);
       $generateCryptKey->setCommandApplication($this->commandApplication);
       $generateCryptKey->run();
@@ -125,19 +130,16 @@ EOF;
     {
       $this->logSection($this->getFullName(), 'Generating of crypt file failed. Regenerate it again.');
     }
-    
+
     // fix permission for common directories
-    $fixPerms = new sfCliProjectPermissionsTask($this->environment, 
-                                                $this->dispatcher, 
-                                                $this->formatter, 
+    $fixPerms = new sfCliProjectPermissionsTask($this->environment,
+                                                $this->dispatcher,
+                                                $this->formatter,
                                                 $this->logger);
     $fixPerms->setCommandApplication($this->commandApplication);
     $fixPerms->run();
 
-    // replace tokens
-    // $this->replaceTokens();
-    
-    $this->logSection($this->getFullName(), 'Done.');    
+    $this->logSection($this->getFullName(), 'Done.');
   }
 
 }
