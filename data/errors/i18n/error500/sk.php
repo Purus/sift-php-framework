9d158e61a3d02eacdaa0d28ceba55814c4a6cce5
Mishal <michi.m@gmail.com>
2013-11-08 12:00:32 +0100

    added slovak translation for 500 and unavailable error page

---
+++ data/errors/i18n/error500/sk.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of the Sift PHP framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+  'Interná chyba servera',
+  'Ospravedlňujeme sa, ale pri vybavovaní Vašej požiadavky došlo k internej chybe servera.',
+  'Chyba bola uložená pre administrátora systému. Ak sa bude chyba zobrazovať aj naďalej, prosím kontaktujte webmastera <a href="mailto:%email%" title="Odeslat email webmasterovi">%email%</a>.',
+  'Ďakujeme za Vašu trpezlivosť.'
+);
---
+++ data/errors/i18n/unavailable/sk.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of the Sift PHP framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+  'Stránky sú dočasne nedostupné',
+  'Ospravedlňujeme sa, ale práve <strong> vykonávame údržbu stránok </strong>. Prosíme skúste to za 30 minút.',
+  'Děkujeme za Vaši trpělivost.'
+);
