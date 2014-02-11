56c2417080c6e72bc19aca99fdb8000f51585552
Michal Moravec <michi.m@gmail.com>
2014-02-11 16:39:21 +0100

    Updated tests

--- test/unit/form/sfFormAriaTest.php
+++ test/unit/form/sfFormAriaTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the Sift PHP framework.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 require_once($_test_dir.'/unit/sfCoreMock.class.php');
 
