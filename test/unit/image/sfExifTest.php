c4a63ed846efec895ab33b47ae1e281a67fe08f7
Michal Moravec <michi.m@gmail.com>
2014-02-14 10:01:04 +0100

    fixed exif test

--- test/unit/image/sfExifTest.php
+++ test/unit/image/sfExifTest.php
@@ -55,46 +55,6 @@ catch(BadMethodCallException $e)
   $t->pass('Call of invalid method throws an exception');
 }
 
-$return = null;
-$output = array();
-exec('exiftool', $output, $return);
-
-if($return)
-{
-  $t->skip('exiftool is not installed in the PATH', 85);
-}
-
-// real test, requires exiftool executable to be installed
-$exif = new sfExif('ExifTool', array('exiftool_executable' => 'exiftool'));
-
-$data = $exif->getData(dirname(__FILE__).'/fixtures/bible.jpg');
-
-$t->isa_ok($data, 'array', 'getData() returned array');
-
-$t->is($data, array(
-    'FileSize' => 387760,
-    'XResolution' => 100,
-    'YResolution' => 100,
-    'ResolutionUnit' => 0,
-), 'getData() returned array');
-
-
-$data = $exif->getData(dirname(__FILE__).'/fixtures/ck.jpg');
-$t->isa_ok($data, 'array', 'getData() returned array');
-$t->isa_ok(isset($data['Orientation']), true, 'getData() returned orientation information');
-
-$data = $exif->getData(dirname(__FILE__).'/fixtures/gps.jpg');
-$t->isa_ok(isset($data['GPSLatitude']) && isset($data['GPSLongitude']), true, 'getData() returned GPS information');
-
-$fields = $exif->getSupportedFields();
-
-foreach($data as $field => $value)
-{
-  $t->isa_ok($fields[$field], 'array', sprintf('The field "%s" is known field.' , $fields[$field]['description']));
-  $humanValue = sfExif::getHumanReadable($field, $value);
-  $t->isa_ok(!empty($humanValue), true, sprintf('Returns human readable format for "%s"', $field));
-}
-
 // 16 images
 $images = array(
     'Landscape_1.jpg' => 1,
@@ -112,29 +72,65 @@ $images = array(
     'Portrait_5.jpg' => 5,
     'Portrait_6.jpg' => 6,
     'Portrait_7.jpg' => 7,
-    'Portrait_8.jpg' => 8,    
+    'Portrait_8.jpg' => 8,
 );
 
-foreach($images as $image => $expectedValue)
-{ 
-  $data = $exif->getData($fixturesDir.'/orientation/'.$image);
-  $t->is($data['Orientation'], $expectedValue, sprintf('Orientation is read successfully and correctly for "%s"', $image));
+$return = null;
+$output = array();
+@exec('exiftool', $output, $return);
+
+if ($return) {
+  $t->skip('exiftool is not installed in the PATH', 85);
+} else {
+    // real test, requires exiftool executable to be installed
+    $exif = new sfExif('ExifTool', array('exiftool_executable' => 'exiftool'));
+
+    $data = $exif->getData(dirname(__FILE__).'/fixtures/bible.jpg');
+
+    $t->isa_ok($data, 'array', 'getData() returned array');
+
+    $t->is($data, array(
+        'FileSize' => 387760,
+        'XResolution' => 100,
+        'YResolution' => 100,
+        'ResolutionUnit' => 0,
+    ), 'getData() returned array');
+
+    $data = $exif->getData(dirname(__FILE__).'/fixtures/ck.jpg');
+    $t->isa_ok($data, 'array', 'getData() returned array');
+    $t->isa_ok(isset($data['Orientation']), true, 'getData() returned orientation information');
+
+    $data = $exif->getData(dirname(__FILE__).'/fixtures/gps.jpg');
+    $t->isa_ok(isset($data['GPSLatitude']) && isset($data['GPSLongitude']), true, 'getData() returned GPS information');
+
+    $fields = $exif->getSupportedFields();
+
+    foreach($data as $field => $value)
+    {
+        $t->isa_ok($fields[$field], 'array', sprintf('The field "%s" is known field.' , $fields[$field]['description']));
+        $humanValue = sfExif::getHumanReadable($field, $value);
+        $t->isa_ok(!empty($humanValue), true, sprintf('Returns human readable format for "%s"', $field));
+    }
+
+    foreach($images as $image => $expectedValue)
+    {
+        $data = $exif->getData($fixturesDir.'/orientation/'.$image);
+        $t->is($data['Orientation'], $expectedValue, sprintf('Orientation is read successfully and correctly for "%s"', $image));
+    }
 }
 
 // test is with native php
-if(!function_exists('exif_read_data'))
-{
+if (!function_exists('exif_read_data')) {
   $t->skip('Exif read data is not installed', 16);
+} else {
+
+    $t->diag('native adapter');
+
+    $exif = new sfExif('native');
+
+    foreach($images as $image => $expectedValue)
+    {
+        $data = $exif->getData($fixturesDir.'/orientation/'.$image);
+        $t->is($data['Orientation'], $expectedValue, sprintf('Orientation is read successfully and correctly for "%s"', $image));
+    }
 }
-
-$t->diag('native adapter');
-
-$exif = new sfExif('native');
-
-foreach($images as $image => $expectedValue)
-{ 
-  $data = $exif->getData($fixturesDir.'/orientation/'.$image);
-  $t->is($data['Orientation'], $expectedValue, sprintf('Orientation is read successfully and correctly for "%s"', $image));
-}
-
-
