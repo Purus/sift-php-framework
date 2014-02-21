a87cb4ba58e64c95a4726a3def10d233e8df34e3
Michal Moravec <michi.m@gmail.com>
2014-02-21 16:14:30 +0100

    Updated request test

--- lib/request/sfRequest.class.php
+++ lib/request/sfRequest.class.php
@@ -366,7 +366,7 @@ abstract class sfRequest implements sfIRequest, Serializable
     }
 
     /**
-     * Returns a request parameter as array of raw strings (without htmlentities)
+     * Returns a request parameter as array of raw strings (without applying htmlentities)
      *
      * @param string $name    Name of the parameter
      * @param mixed  $default Optional. Default value. Default: NULL
@@ -376,6 +376,8 @@ abstract class sfRequest implements sfIRequest, Serializable
      */
     public function getRawStringArray($name, $default = null, $ns = null)
     {
+        var_dump($this->parameterHolder->get($name, $default, $ns));
+        var_dump(get_magic_quotes_gpc());
         return sfInputFilters::toRawStringArray($this->parameterHolder->get($name, $default, $ns));
     }
 
